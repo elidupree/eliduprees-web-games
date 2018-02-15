f7bc4257907f3cbd76af74e8184b7584e1c70025
Eli Dupree <vcs@elidupree.com>
2018-02-15 12:37:24 -0500

    Initial commit

---
+++ src/bin/the_path.rs
@@ -0,0 +1,12 @@
+#![recursion_limit="128"]
+
+extern crate eliduprees_web_games;
+
+#[macro_use]
+extern crate stdweb;
+extern crate nalgebra;
+extern crate rand;
+
+fn main() {
+  println!("not yet implemented :-P");
+}
