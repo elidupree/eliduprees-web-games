95b00c4b85dbbdcc17907ec98bb49a764c210ec4
Eli Dupree <vcs@elidupree.com>
2018-10-24 17:21:16 -0400

    More refining the flow rate propagation system

--- src/bin/my_factory_has_a_trillion_machines/simulation.rs
+++ src/bin/my_factory_has_a_trillion_machines/simulation.rs
@@ -7,7 +7,7 @@ type Number = i64;
 const MAX_COMPONENTS: usize = 32;
 const RATE_DIVISOR: Number = 2*2*2*2*2*2 * 3*3*3 * 5*5;
 const MAX_MACHINE_INPUTS: usize = 8;
-type Inputs<T> = ArrayVec <[Number, MAX_MACHINE_INPUTS]>
+type Inputs<T> = ArrayVec <[Number; MAX_MACHINE_INPUTS]>;
 type Position = Vector2 <Number>;
 
 
@@ -22,13 +22,13 @@ pub trait Machine: Clone {
   // property: these are consistent with each other
   fn max_output_rates (&self, input_rates: Inputs <Number>)->Inputs <Number>;
   // note: this API implies that mergers must have fixed ratios
-  fn min_input_rates (&self, output_rates: Inputs <Number>)->Inputs <Number>;
+  fn min_input_rates_to_produce (&self, output_rates: Inputs <Number>)->Inputs <Number>;
   
   // property: if inputs don't change, current_output_rates doesn't change before next_output_change_time
   // property: when there is no next output change time, current_output_rates is equivalent to max_output_rates
   // maybe some property that limits the total amount of rate changes resulting from a single change by the player?
   fn with_inputs_changed (&self, old_state: Self::MaterialsState, when: Number, input_patterns: Inputs <FlowPattern>)->Self::MaterialsState;
-  fn current_outputs_and_next_change (&self, state: Self::MaterialsState, input_patterns: Inputs <FlowPattern>)->(Inputs <FlowPattern>, Option <(Number, Self::MaterialsState)>;
+  fn current_outputs_and_next_change (&self, state: Self::MaterialsState, input_patterns: Inputs <FlowPattern>)->(Inputs <FlowPattern>, Option <(Number, Self::MaterialsState)>);
 
 }
 
@@ -48,7 +48,8 @@ impl FlowPattern {
     self.num_disbursed_before (time + 1) - self.num_disbursed_before (time)
   }
   pub fn num_disbursed_before (&self, time: Number)->Number {
-    ((time - self.start_time)*self.rate + RATE_DIVISOR - 1)/RATE_DIVISOR
+    if time <= self.start_time {return 0;}
+    1 + ((time - self.start_time)*self.rate)/RATE_DIVISOR
   }
   pub fn num_disbursed_between (&self, range: [Number; 2])->Number {
     self.num_disbursed_before (range [1]) - self.num_disbursed_before (range [0])
@@ -58,7 +59,7 @@ impl FlowPattern {
   }
 }
 
-
+/*
 pub fn entire_future (machines:, max_time: time) {
   for machine_info in machine_dag.forwards_iter_mut() {
     let mut simulation_time = now;
@@ -80,119 +81,102 @@ pub fn entire_future (machines:, max_time: time) {
     }
   }
   changes map
+}*/
+
+#[derive (Clone, Hash, Debug)]
+struct StandardMachineInput {
+  cost: Number,
 }
 
-
-start ConveyorMaterialsState {
-  current_output: FlowPattern
+#[derive (Clone, Hash, Debug)]
+struct StandardMachineOutput {
+  
 }
 
-impl Machine for Conveyor {
-  fn num_inputs (&self)->usize {1}
-  fn num_outputs (&self)->usize {1}
-  type MaterialsState = ConveyorMaterialsState;
+#[derive (Clone, Hash, Debug)]
+struct StandardMachine {
+  inputs: Inputs <StandardMachineInput>,
+  outputs: Inputs <StandardMachineOutput>,
+  min_output_cycle_length: Number,
+}
+
+#[derive (Clone, Hash, Debug)]
+struct MachineMaterialsStateInput {
+  storage_at_last_pattern_start: Number,
+}
+
+#[derive (Clone, Hash, Debug)]
+struct MachineMaterialsState {
+  current_output_pattern: FlowPattern,
+  inputs: Inputs <MachineMaterialsStateInput>,
+}
+
+impl StandardMachine {
+  fn max_output_rate (&self)->Number {
+    RATE_DIVISOR/map_state.min_output_cycle_length
+  }
+  fn max_output_rate_with_inputs <I: IntoIterator <Item = Number>> (&self, input_rates: I)->Number {
+    let mut ideal_rate = self.max_output_rate();
+    for (rate, input) in input_rates.into_iter().zip (self.inputs.iter()) {
+      ideal_rate = min (ideal_rate, rate/input.cost);
+    }
+    ideal_rate
+  }
+  fn min_output_rate_to_produce <I: IntoIterator <Item = Number>> (&self, output_rates: I)->Number {
+    output_rates.iter().max().unwrap_or(self.max_output_rate())
+  }
+}
+
+impl Machine for StandardMachine {
+  fn num_inputs (&self)->usize {self.inputs.len()}
+  fn num_outputs (&self)->usize {self.outputs.len()}
   
-  fn max_output_rates (&self, input_rates: Inputs <Number>)->Inputs <Number> {input_rates}
-  fn min_input_rates (&self, output_rates: Inputs <Number>)->Inputs <Number> {output_rates}
+  fn max_output_rates (&self, input_rates: Inputs <Number>)->Inputs <Number> {
+    let ideal_rate = self.max_output_rate_with_inputs (input_rates);
+    self.outputs.iter().map (| _output | ideal_rate).collect()
+  }
+  fn min_input_rates_to_produce (&self, output_rates: Inputs <Number>)->Inputs <Number> {
+    let ideal_rate = self.min_output_rate_to_produce (output_rates);
+    self.inputs.iter().map (| input | ideal_rate*input.cost).collect()
+  }
   
-  fn with_inputs_changed (&self, old_state: Self::MaterialsState, when: Number, input_patterns: Inputs <FlowPattern>)->Self::MaterialsState {
+  fn with_inputs_changed (&self, old_state: MachineMaterialsState, when: Number, input_patterns: Inputs <FlowPattern>)->MachineMaterialsState {
     old_state
   }
-  fn current_outputs_and_next_change (&self, state: Self::MaterialsState, input_patterns: Inputs <FlowPattern>)->(Inputs <FlowPattern>, Option <(Number, Self::MaterialsState)>
+  fn current_outputs_and_next_change (&self, state: MachineMaterialsState, input_patterns: Inputs <FlowPattern>)->(Inputs <FlowPattern>, Option <(Number, MachineMaterialsState)>)
  {
-    let input = input_patterns [1];
-    let output = FlowPattern {start_time: input.start_time + 1, rate: input.rate};
+    let ideal_rate = self.max_output_rate_with_inputs (input_patterns.iter().map (| pattern | pattern.rate));
+    let last_change_time = input_patterns.map (| pattern | pattern.start_time).
+    let mut when_enough_inputs_to_begin_output = last_change;
+    for ((pattern, input), input_state) in input_patterns.iter().zip (self.inputs.iter()).zip (state.inputs.iter()) {
+      let enough_to_start_amount = input.cost + 1;
+      let storage_at_last_change =
+        input_state.storage_at_pattern_start
+        + pattern.num_disbursed_before (last_change_time)
+        - input.cost*state.current_output_pattern.num_disbursed_between ([pattern.start_time, last_change_time]);
+      let min_start_time = last_change_time + pattern.time_to_disburse_at_least (pattern.start_time, enough_to_start_amount - storage_at_last_change);
+      when_enough_inputs_to_begin_output = max (time_to_begin_output, min_start_time);
+    }
+    let most_recent_output = state.current_output_pattern.last_disbursement_before (last_change_time).unwrap_or (last_change_time - self.min_output_cycle_length);
+    let time_to_begin_output = max (when_enough_inputs_to_begin_output, most_recent_output + self.min_output_cycle_length);
+    let output = FlowPattern {start_time: time_to_begin_output, rate: ideal_rate};
     
-    let next_change = if output == state.current_output {
+    let next_change = if output == state.current_output_pattern {
       None
     } else {
       Some ((input.start_time + 1, ConveyorMaterialsState {
         current_output: output
       }))
     };
-     
-    (array_vec! [state.current_output], next_change)
+    
+    let current_outputs = self.outputs.iter().map (| _output | {
+      FlowPattern {start_time: state.current_output_pattern.start_time + 1, rate: state.current_output_pattern.rate}
+    }).collect();
+    (current_outputs, next_change)
   }
 }
 
 
-
-
-
-
-enum MachineInputStorage {
-  Unsettled {current_amount: Number},
-  Equilibrium,
-}
-
-struct MachineInputState {
-  input: FlowPattern,
-  storage: MachineInputStorage,
-}
-
-struct MachineMaterialsState {
-  output: FlowPattern,
-  inputs: ArrayVec <[MachineInputState; MAX_MACHINE_INPUTS]>,
-}
-
-struct MachineWithState {
-  map_state: &MachineMapState,
-  materials: &mut MachineMaterialsState,
-}
-
-pub fn machine_step (now: Number, machine: MachineWithState, outputs: &mut [(MachineWithState, usize, usize)]) {
-  inputs_are_equilibrium = machine.materials.inputs.all (| input | match input {Equilibrium {..} => true,_=> false});
-  
-  let mut can_start_ideal_rate = true;
-  let mut ideal_rate = MAX_CYCLE_LENGTH/map_state.min_output_cycle_length;
-  for (material_input, map_input) in machine.materials.inputs.iter().zip (machine.map_state.inputs.iter()) {
-    ideal_rate = min (ideal_rate, material_input.input.rate/map_input.cost);
-    match input.storage {
-      Unsettled {current_amount} => {
-        let capacity = map_input.capacity();
-        current_amount += material_input.input.rate.num_disbursed_at_time (now);
-        if current_amount < capacity {
-          can_start_ideal_rate = false;
-        }
-      }
-      _=>()
-    }
-  }
-  
-  if ideal_rate != machine.materials.output.rate && now >= machine.materials.output.last_disbursement_time() + map_state.min_output_cycle_length  {
-    let actual_rate = if can_start_ideal_rate {ideal_rate} else {0};
-    if actual_rate != machine.materials.output.rate {
-      machine.materials.output = FlowPattern (start_time: now, rate: actual_rate};
-      for (material_input, map_input) in machine.materials.inputs.iter().zip (machine.map_state.inputs.iter()) {
-        material_input.storage.either_rate_changed(now);
-      }  
-    }
-  }
-  
-  for (receiver_input, new_flow_rate) in outputs.iter_mut().zip (machine.map_state.output_patterns (machine.materials.output)) {
-    if now >= new_flow_rate.start_time - 1 {
-      receiver_input.set_rate (new_flow_rate);
-    }
-  }
-  
-  for (material_input, map_input) in machine.materials.inputs.iter().zip (machine.map_state.inputs.iter()) {
-    match input.storage {
-      Unsettled {current_amount} => {
-        let last_rate_change_time = min (material_input.input.start_time, machine.materials.output.start_time);
-        let capacity = map_input.capacity();
-        current_amount -= map_input.cost*machine.materials.output.num_disbursed_at_time (now) ;
-        if current_amount > capacity {
-          current_amount = capacity;
-        }
-        let combined_cycle_length = num::integer::lcm (material_input.input.cycle_length(), machine.materials.output.cycle_length());
-        if now == last_rate_change_time + combined_cycle_length {
-          input.storage = MachineInputStorage::Equilibrium;
-        }
-      }
-      _=>()
-    }
-  }
-}
 
 
 
@@ -226,20 +210,4 @@ pub struct Map {
 }
 
 
-
-fn step_component (component:) {
-  match component.component_type {
-    ComponentType::Singular (SingularComponentType::Conveyor) => {
-      destination.push_material (mem::replace (&mut component.carried, None));
-    },
-    
-  }
-}
-
-
-
-fn step (component_graph: ?????) {
-  for component in component_graph.iterate_from_lasts() {
-    component.step();
-  }
-}
+and
