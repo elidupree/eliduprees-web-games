27b339d3fd7ba8ee648a58612dd35d03b6e12d38
elidupree <vcs@elidupree.com>
2021-06-15 17:17:34 -0400

    fleshed out behavior of holding multiple keys

--- media/web-games/deck-of-unhealthy-defense-mechanisms-init.js
+++ media/web-games/deck-of-unhealthy-defense-mechanisms-init.js
@@ -6,6 +6,26 @@ import init, { rust_init, rust_do_frame, }
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 
+const action_keys = {
+  z: "InteractLeft",
+  x: "InteractRight",
+  c: {"PlayCard": 0},
+  v: {"PlayCard": 1},
+  b: {"PlayCard": 2},
+  n: {"PlayCard": 3},
+  m: {"PlayCard": 4},
+};
+const direction_keys = {
+  w: ["vertical", -1],
+  ArrowUp: ["vertical", -1],
+  a: ["horizontal", -1],
+  ArrowLeft: ["horizontal", -1],
+  s: ["vertical", 1],
+  ArrowDown: ["vertical", 1],
+  d: ["horizontal", 1],
+  ArrowRight: ["horizontal", 1],
+};
+
 var dpr = window.devicePixelRatio || 1.0;
 var width = 800;
 var height = 800;
@@ -36,56 +56,47 @@ async function run() {
   await init();
   rust_init();
 
-  let horizontal_intent = 0;
-  let vertical_intent = 0;
-  let action_intent = null;
-  let action_keys = ["z","x","c","v","b","n","m"];
+  const movement_intents = {
+    horizontal: [0],
+    vertical: [0],
+  };
+  let action_intents = [];
   document.body.addEventListener("keydown", (event) => {
     const key = event.key;
-    if (key === "w" || key === "ArrowUp") {
-      vertical_intent = -1;
+    if (direction_keys[key] !== undefined) {
+      const [dimension, direction] = direction_keys[key];
+      if (!movement_intents[dimension].includes(direction)) {
+        // in movement, the latest is given priority (pressing right overrides left)
+        movement_intents[dimension].unshift(direction);
+      }
     }
-    if (key === "a" || key === "ArrowLeft") {
-      horizontal_intent = -1;
-    }
-    if (key === "s" || key === "ArrowDown") {
-      vertical_intent = 1;
-    }
-    if (key === "d" || key === "ArrowRight") {
-      horizontal_intent = 1;
-    }
-    if (action_keys.includes(key)) {
-      action_intent = key;
+    if (action_keys[key] !== undefined) {
+      if (!action_intents.includes(key)) {
+        // for actions, the first is given priority (you can't interrupt yourself)
+        action_intents.push(key);
+      }
     }
   });
   document.body.addEventListener("keyup", (event) => {
     const key = event.key;
-    if ((key === "w" || key === "ArrowUp") && vertical_intent === -1) {
-      vertical_intent = 0;
+    if (direction_keys[key] !== undefined) {
+      const [dimension, direction] = direction_keys[key];
+      movement_intents[dimension] = movement_intents[dimension].filter(i => i !== direction);
     }
-    if ((key === "a" || key === "ArrowLeft") && horizontal_intent === -1) {
-      horizontal_intent = 0;
-    }
-    if ((key === "s" || key === "ArrowDown") && vertical_intent === 1) {
-      vertical_intent = 0;
-    }
-    if ((key === "d" || key === "ArrowRight") && horizontal_intent === 1) {
-      horizontal_intent = 0;
-    }
-    if (action_intent === key) {
-      action_intent = null;
+    if (action_keys[key] !== undefined) {
+      action_intents = action_intents.filter(i => i !== key);
     }
   });
 
   function frame(time) {
     window.requestAnimationFrame(frame);
     let intent;
-    if (action_intent === null) {
-      intent = {"Move": [horizontal_intent, vertical_intent]};
+    if (action_intents.length === 0) {
+      intent = {"Move": [movement_intents.horizontal[0], movement_intents.vertical[0]]};
     } else {
-      intent = {"Interact": "InteractLeft"};
+      intent = {"Interact": action_keys[action_intents[0]]};
     }
-    document.getElementById("debug").innerText = action_intent;
+    document.getElementById("debug").innerText = JSON.stringify(action_intents);
     rust_do_frame(time, {intent});
   }
   window.requestAnimationFrame(frame);
